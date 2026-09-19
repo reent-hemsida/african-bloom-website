bb472cecef64fe07b25c2387fc374d38d3db4813
reent-hemsida <nikitapino76@gmail.com>
2026-09-19 15:36:30 +0200

    Add files via upload

---
+++ varieties-data.js
@@ -0,0 +1,421 @@
+/* Variety list for the Varieties page.
+   One variety per line:  Name | category | sub-type | color
+   category: r roses, g garden roses, s spray roses, c carnations, a alstroemeria,
+             u ranunculus, y gypsophila, o other flowers
+   sub-type (optional): gl garden-like spray, hs hashtag spray, cl Cloni, el Elegance,
+             ch chrysanthemum, hy hydrangea, de delphinium, st stock, an anemone
+   color (optional): number from the COLORS list in varieties.html (0 Bicolor ... 18 Yellow)
+   To add a real photo for a variety, add a fifth part: file name, e.g.  Freedom|r||16|freedom.jpg */
+window.VARIETY_DATA = `Bach|c||7
+Cotton Mist|g||14
+Celia X-Press|s|gl|14
+Zafiro|c||15
+Virgilio|c||16
+Violet Pixel|c||15
+Uyuni|c||17
+Siren|c||8
+Sassicaia|c||3
+Polimnia|c||4
+Polar Route|c||17
+Pink Pixel|c||14
+Petra|c||13
+Pastel Crimea|c||14
+Nobbio Crochet|c||0
+Nobbio Babel|c||15
+Merletto Purple|c||0
+Marte|c||16
+Jungle|c||6
+Epic|c||16
+Crimea|c||13
+Corvette|c||14
+Cocuy|c||17
+Clearwater|c||8
+Charisma|c||14
+Brut|c||13
+Ariadna|c||14
+Cloni Success Vanilla|u|cl|4
+Elegance Malva|u|el|8
+Elegance Rosa Chiaro|u|el|14
+Veronicas|o||11
+Hypericum|o||11
+Sunflower Sunrich|o||18
+Delphinium Andes White|o|de|
+Hashtag Medium Pink|s|hs|14
+Juicy X-Pression|g||13
+Gypsophila Xlence White|y||17
+Hydrangea Jumbo White|o|hy|17
+Gypsophila Xlence Light Blue|y||9
+Gypsophila Xlence Light Pink|y||10
+Delphinium Serene|o|de|8
+Disbud Ball Purple|o|ch|15
+Disbud Ball Yellow|o|ch|18
+Disbud Ball Green|o|ch|6
+Disbud Ball Pink|o|ch|14
+Disbud Ball White|o|ch|17
+Cremons Bronze|o|ch|2
+Cremons Red|o|ch|16
+Cremons Purple|o|ch|15
+Cremons Peach|o|ch|13
+Cremons Orange|o|ch|12
+Cremons Lavender|o|ch|8
+Cremons Yellow|o|ch|18
+Cremons Pink|o|ch|14
+Cremons White|o|ch|17
+Football Mum Bronze|o|ch|2
+Football Mum White|o|ch|17
+Spider Mum Purple|o|ch|15
+Spider Mum Yellow|o|ch|18
+Spider Mum Green|o|ch|6
+Spider Mum Pink|o|ch|14
+Spider Mum White|o|ch|17
+Sunflower Vincent’s|o||18
+Solidago Golden Glory|o||18
+Stock White|o|st|17
+Stock Lavender|o|st|8
+Stock Hot Pink|o|st|7
+Stock Dark Purple|o|st|15
+Lisianthus Assorted|o||11
+Hydrangea Premium White|o|hy|17
+Hydrangea Tinted Pink|o|hy|14
+Hydrangea Tinted Light Yellow|o|hy|18
+Hydrangea Tinted Light Orange|o|hy|12
+Hydrangea Tinted Light Purple|o|hy|15
+Hydrangea Tinted Light Pink|o|hy|14
+Hydrangea Tinted Light Peach|o|hy|13
+Hydrangea Tinted Hot Pink|o|hy|7
+Hydrangea Premium Lemon|o|hy|6
+Hydrangea Tinted Burgundy|o|hy|16
+Hydrangea Tinted Dark Blue|o|hy|1
+Hydrangea Tinted Gold|o|hy|5
+Hydrangea Tinted Light Lime Green|o|hy|6
+Hydrangea Tinted Light Red|o|hy|16
+Hydrangea Tinted Orange|o|hy|12
+Hydrangea Tinted Purple|o|hy|15
+Hydrangea Tinted Rainbow|o|hy|11
+Hydrangea Tinted Red|o|hy|16
+Hydrangea Tinted Yellow|o|hy|18
+Hydrangea Mini Emerald|o|hy|6
+Hydrangea Mini Green|o|hy|6
+Hydrangea Mini Lemon|o|hy|6
+Hydrangea Premium Emerald|o|hy|6
+Hydrangea Select Emerald|o|hy|6
+Hydrangea Shocking Blue|o|hy|1
+Hydrangea Premium Blue|o|hy|1
+Gypsophila Xlence Rainbow|y||11
+Delphinium Sky Waltz|o|de|1
+Delphinium Sea Waltz|o|de|1
+Bells of Ireland|o||6
+Winterfell|a||17
+White Focus|a||17
+Whistler|a||17
+Testarossa|a||16
+Sweetheart|a||0
+Sunny Lady|a||18
+Strawberry Snow|a||0
+Stratus|a||14
+Stephanie|a||14
+Snow Time|a||17
+Secret|a||7
+Seattle|a||14
+Savanna|a||12
+Rosewood|a||14
+Rome|a||7
+Romance|a||16
+Red Queen|a||16
+Primadonna|a||14
+Pompeii|a||15
+Pierrot|a||0
+Party|a||12
+Orange Queen|a||12
+Nora|a||14
+Nirvana|a||16
+Mistral|a||17
+Mila|a||14
+Melrose|a||7
+Maracana|a||15
+Luzon|a||17
+Lucia|a||7
+Lousiana|a||8
+Limelight|a||6
+Libya|a||13
+Jennifer|a||17
+Intenz|a||7
+Hot Shot|a||0
+Hot Pepper|a||0
+Himalaya|a||17
+Gold|a||18
+Galeras|a||16
+Galaxy|a||8
+Florida|a||18
+Florentina|a||8
+Elegance|a||14
+Dubai|a||14
+Dirty Dancing|a||4
+Debby|a||14
+Corsica|a||0
+Cordoba|a||0
+Concorde|a||17
+Cleo|a||15
+Cinnamon|a||12
+Champagne|a||4
+Casanova|a||7
+Canyon|a||13
+Canary|a||18
+Bounty|a||17
+Bohemian|a||0
+Bali|a||0
+Bahia|a||16
+Badalona|a||18
+Antaris|a||8
+Andalusia|a||0
+Amposta|a||14
+Amatista|a||15
+Alice|a||14
+Alegria|a||7
+Aaron|a||15
+Elegance Viola|u|el|15
+Elegance Salmone|u|el|13
+Elegance Rosso|u|el|16
+Elegance Rosa|u|el|14
+Elegance Milka|u|el|15
+Elegance Hot Pink|u|el|7
+Elegance Giallo|u|el|18
+Elegance Clementine|u|el|12
+Elegance Bianco|u|el|17
+Cloni Success Nebbia|u|cl|14
+Cloni Success Luce|u|cl|18
+Cloni Success Lady|u|cl|14
+Cloni Success Hanoi|u|cl|14
+Cloni Success Favola|u|cl|14
+Cloni Success Blush|u|cl|14
+Cloni Pon Pon Igloo|u|cl|0
+Wink Coral Kiss|s|gl|14
+Wink Allure|s|gl|4
+White Majolika|s||17
+White Flow|s|gl|17
+Wedding Sensation|s||14
+Wedding Rosever|s|gl|14
+Vintage Rosever|s||15
+Verona|s||16
+That’s Red Jewel|s|gl|16
+Sweet Jewel|s||0
+Sweet Dreams|s||17
+Suspiro|s||14
+Star Blush|s||14
+Snowy Jewel|s||17
+Snowflake|s||17
+Silver Mikado|s||8
+Shining Star|s||17
+Saona|s||12
+Rubicon|s||16
+Royal Porcelina|s||14
+Purple Irishka|s||7
+Portrait|s||8
+Polar Sensation|s||17
+Pink Irishka|s||14
+Peachy Pink Jewel|s||14
+Party Trendsetter|s|gl|7
+Orange Trendsetter|s|gl|12
+New Moon|s||4
+Morning Star|s||13
+Lavender Irishka|s||8
+Hot Pink Trendsetter|s|gl|7
+Hot Magenta Jewel|s||7
+Green Rosever|s||6
+Gem Star|s||7
+Garden Bridal Jewel|s|gl|14
+Floreana|s||17
+Fire Up|s||7
+Fibonacci Yantra|s|gl|0
+Fibonacci Rosso|s|gl|16
+Fibonacci Nautilus|s|gl|14
+Fibonacci Gioconda|s|gl|18
+Fibonacci Aureus|s|gl|13
+Elba|s||14
+Creta|s||13
+Cream Flow|s|gl|4
+Constellation|s|gl|14
+Candy Jewel|s||14
+B.S Volans|s||13
+B.S Vela|s||14
+B.S Taurus|s||12
+B.S Scorpio|s||14
+B.S Sagitta|s||8
+B.S Cygnus|s||14
+B.S Cassiopeia|s||15
+B.S Bonbon|s||14
+B.S Aries|s||16
+B.S Leo|s||18
+B.S Capricorn|s||13
+Bright Sensation|s||12
+Boomie Gardens|s|gl|14
+Boomerang|s|gl|14
+Blue Moon|s||8
+Big Dipper|s||14
+Bellalinda Cerise|s||7
+Be True|s|gl|14
+Be Strong|s|gl|4
+Be Loyal|s|gl|4
+Be Loving|s|gl|14
+Be Gracious|s|gl|7
+Be Generous|s|gl|4
+Arctic Flow|s|gl|17
+Flow Pink|s|gl|14
+Baby Flow|s|gl|14
+Apricot Jewel|s||13
+Hashtag Treasure|s|hs|16
+Hashtag Pink|s|hs|14
+Hashtag Paradise|s|hs|18
+Hashtag Light Pink|s|hs|14
+Hashtag Hot Pink|s|hs|7
+Exotix Mint|g||6
+Sweet Beat|r||14
+Wildcat|r||0
+White Shimmer|r||17
+Violet Hill|r||8
+Vendela|r||4
+V.I. Pink|r||7
+Twilight|r||12
+Topaz|r||7
+Tiffany|r||13
+Tibet|r||17
+Sweetness|r||0
+Sweet Unique|r||14
+Sweet Escimo|r||14
+Sweet Cake|r||14
+Sweet Bliss|r||14
+Summer Light|r||0
+Star Platinum|r||8
+Snowbliss|r||17
+Silantoi|r||0
+Shocking Blue|r||8
+Shimmer|r||13
+Rosita Vendela|r||14
+Roseberry|r||7
+Rhoslyn|r||14
+Redvolution|r||16
+Rediant|r||16
+Red Panther|r||16
+Quicksand|r||4
+Queen Berry|r||7
+Proud|r||17
+Pomarosa|r||14
+Polo|r||17
+Playa Blanca|r||17
+Pink Mondial|r||14
+Pink Floyd|r||7
+Piacere|r||8
+Phoenix|r||13
+Peach Avalanche|r||13
+Pastella|r||14
+Pandora|r||0
+Paloma|r||0
+Orange Crush|r||12
+Opala|r||14
+Ocean Song|r||8
+Nina|r||12
+Nexus|r||12
+New Face|r||12
+Nena|r||14
+Nectarine|r||13
+Nautica|r||14
+Mother of Pearl|r||14
+Moody Blues|r||8
+Mondial|r||17
+Momentum|r||18
+Miss Piggy|r||14
+Mandala|r||14
+Mamma Mia|r||16
+Magic Times|r||0
+Lola|r||7
+Lighthouse|r||18
+Kahala|r||0
+Joy|r||12
+Jet Set|r||14
+Jessika|r||14
+Hotspot|r||7
+Hot Explorer|r||7
+Highlight|r||17
+High & Yellow Magic|r||18
+High & Magic|r||0
+Hard Rock|r||14
+Green Romance|r||6
+Grace Kelly|r||7
+Govinda|r||8
+Gotcha|r||7
+Goldfinch|r||18
+Geraldine|r||14
+Full Monty|r||7
+Frutteto|r||0
+Freedom|r||16
+Free Spirit|r||12
+Fortune|r||16
+Flirty|r||14
+Finally|r||16
+Felicity|r||13
+Fascination|r||14
+Fancy Dreams|r||4
+Faith|r||14
+Explorer|r||16
+Esperance|r||0
+Encanto|r||12
+Deep Purple|r||8
+Crème de la crème|r||4
+Coral Reef|r||13
+Cool Water|r||8
+Cool Down|r||8
+Cherry Brandy|r||0
+Carnelian|r||12
+Candlelight|r||17
+Cancun|r||18
+Cabaret|r||0
+Bumblebee|r||18
+Bromo|r||12
+Born Beauty|r||7
+Blueberry|r||8
+Blessing|r||14
+Bikini|r||18
+Be Sweet|r||14
+Avalanche|r||17
+Atomic|r||0
+Alba|r||17
+Absolut in Pink|r||14
+White O’hara|g||17
+Vicky Gardens|g||4
+Veggie|g||6
+Tip Top|g||13
+Tie-Dye|g||13
+Sunset X-Pression|g||0
+Scandal|g||8
+Queen’s Crown|g||8
+Purple Crown|g||8
+Princess Crown|g||13
+Pink X-Pression|g||14
+Peonikiss|g||14
+Newsflash|r||0
+Moonstone|g||4
+Melon X-Pression|g||13
+Mayra’s Peach|g||13
+Mayra’s Hot Pink|g||7
+Mandarin X-Pression|g||13
+Limonada|g||6
+Kings Cross|g||12
+Journey|g||6
+Hearts|g||16
+Garden Spirit|g||4
+Fatima Gardens|g||4
+Dynamic|g||4
+Dragonfly|g||12
+Crystal Flame|g||13
+Cream X-pression|g||4
+Country Secret|g||14
+Country Home|g||13
+Country Candy|g||0
+Country Blues|g||8
+Cotton X-Pression|g||4
+Candy X-Pression|g||14
+Aurora Gardens|g||4
+Apple Jack|g||0
+Antonia Gardens|g||4
+Angel X-Pression|g||17
+Mistral Panda Blush|o|an|17`;
